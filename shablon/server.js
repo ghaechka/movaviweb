edeb3186bb02120bc73277d44d197cf42d7cc56e
ghaechka <59920101+ghaechka@users.noreply.github.com>
2021-02-28 20:02:58 +0700

    28.02

--- shablon/server.js
+++ shablon/server.js
@@ -13,10 +13,34 @@ const server = http.createServer((req, res) => {
     // console.log(url);
     if (url != '/favicon.ico') {
 
+        if (req.url.endsWith('.css')) {
+            let cssFile = req.url.slice(1);
+
+            fs.readFile (cssFile, (err, data) => {
+                if (err) throw err;
+
+                res.setHeader('Content-Type', 'text/css');
+                res.statusCode = 200;
+                res.write(data);
+                res.end()
+            })
+        }
+        else if (req.url.endsWith('.js')){
+            let jsFile = req.url.slice(1);
+
+            fs.readFile (jsFile, (err, data) => {
+                if (err) throw err;
+
+                res.setHeader('Content-Type', 'text/javascript');
+                res.statusCode = 200;
+                res.write(data);
+                res.end()
+        }
+
         fs.readFile(url.substr(1, url.length)+'.html', function (err, data) {
             if (!err) {
                 res.writeHead(200, { 'Content-Type': 'text/html', });
-                data = data.toString().replace(/\{\{name\}\}/g, "Sofie");
+                data = data.toString().replace("{{name}}", "Sofie");
                 // res.write(data)
                 res.end(data);
             } else {
